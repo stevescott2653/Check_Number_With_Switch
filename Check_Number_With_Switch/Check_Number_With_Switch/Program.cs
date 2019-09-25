5b9628b4bd58ca0adc8ec8ef7c66ac683efb5593
Steven Scott <stevescott2653@gmail.com>
2019-09-25 15:08:13 -0400

    Add files via upload

---
+++ Check_Number_With_Switch/Check_Number_With_Switch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Number_With_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+                case 10: Console.WriteLine("It is 10"); break;
+                case 20: Console.WriteLine("It is 20"); break;
+                case 30: Console.WriteLine("It is 30"); break;
+                default: Console.WriteLine("Not 10, 20, or 30"); break;
+            }
+        }
+    }
+}
